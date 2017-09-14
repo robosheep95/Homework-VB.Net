7250aa0e86bcbba5bec6e95ccaae4f014b8cb925
Taylor Scafe <scafetaylor@gmail.com>
2017-09-14 13:54:22 -0500

    Created Checking App Project

---
+++ 04 CheckingApp/CheckingApp/Form1.Designer.vb
@@ -0,0 +1,209 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMainMenu
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.rbnService = New System.Windows.Forms.RadioButton()
+        Me.rbnDeposit = New System.Windows.Forms.RadioButton()
+        Me.rbnCheck = New System.Windows.Forms.RadioButton()
+        Me.tbxInput = New System.Windows.Forms.TextBox()
+        Me.lblInput = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lblTransactions = New System.Windows.Forms.Label()
+        Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel()
+        Me.tblTransaction = New System.Data.DataSet()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.tblTransaction, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.rbnService)
+        Me.GroupBox1.Controls.Add(Me.rbnDeposit)
+        Me.GroupBox1.Controls.Add(Me.rbnCheck)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(238, 47)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Deposit Type"
+        '
+        'rbnService
+        '
+        Me.rbnService.AutoSize = True
+        Me.rbnService.Location = New System.Drawing.Point(135, 19)
+        Me.rbnService.Name = "rbnService"
+        Me.rbnService.Size = New System.Drawing.Size(98, 17)
+        Me.rbnService.TabIndex = 2
+        Me.rbnService.TabStop = True
+        Me.rbnService.Text = "Service Charge"
+        Me.rbnService.UseVisualStyleBackColor = True
+        '
+        'rbnDeposit
+        '
+        Me.rbnDeposit.AutoSize = True
+        Me.rbnDeposit.Location = New System.Drawing.Point(68, 19)
+        Me.rbnDeposit.Name = "rbnDeposit"
+        Me.rbnDeposit.Size = New System.Drawing.Size(61, 17)
+        Me.rbnDeposit.TabIndex = 1
+        Me.rbnDeposit.TabStop = True
+        Me.rbnDeposit.Text = "Deposit"
+        Me.rbnDeposit.UseVisualStyleBackColor = True
+        '
+        'rbnCheck
+        '
+        Me.rbnCheck.AutoSize = True
+        Me.rbnCheck.Location = New System.Drawing.Point(6, 19)
+        Me.rbnCheck.Name = "rbnCheck"
+        Me.rbnCheck.Size = New System.Drawing.Size(56, 17)
+        Me.rbnCheck.TabIndex = 0
+        Me.rbnCheck.TabStop = True
+        Me.rbnCheck.Text = "Check"
+        Me.rbnCheck.UseVisualStyleBackColor = True
+        '
+        'tbxInput
+        '
+        Me.tbxInput.Location = New System.Drawing.Point(116, 65)
+        Me.tbxInput.Name = "tbxInput"
+        Me.tbxInput.Size = New System.Drawing.Size(129, 20)
+        Me.tbxInput.TabIndex = 1
+        '
+        'lblInput
+        '
+        Me.lblInput.AutoSize = True
+        Me.lblInput.Location = New System.Drawing.Point(12, 68)
+        Me.lblInput.Name = "lblInput"
+        Me.lblInput.Size = New System.Drawing.Size(102, 13)
+        Me.lblInput.TabIndex = 2
+        Me.lblInput.Text = "Transaction Amount"
+        Me.lblInput.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(13, 107)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 3
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnClear.Location = New System.Drawing.Point(94, 107)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.btnExit.Location = New System.Drawing.Point(175, 107)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(38, 151)
+        Me.Label1.MaximumSize = New System.Drawing.Size(200, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(194, 32)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Overdrafting your account will result in a service charge of $10"
+        '
+        'lblTransactions
+        '
+        Me.lblTransactions.AutoSize = True
+        Me.lblTransactions.Location = New System.Drawing.Point(291, 20)
+        Me.lblTransactions.Name = "lblTransactions"
+        Me.lblTransactions.Size = New System.Drawing.Size(68, 13)
+        Me.lblTransactions.TabIndex = 8
+        Me.lblTransactions.Text = "Transactions"
+        '
+        'TableLayoutPanel1
+        '
+        Me.TableLayoutPanel1.ColumnCount = 2
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Location = New System.Drawing.Point(294, 36)
+        Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+        Me.TableLayoutPanel1.RowCount = 2
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+        Me.TableLayoutPanel1.Size = New System.Drawing.Size(303, 49)
+        Me.TableLayoutPanel1.TabIndex = 9
+        '
+        'tblTransaction
+        '
+        Me.tblTransaction.DataSetName = "Transaction"
+        '
+        'frmMainMenu
+        '
+        Me.AcceptButton = Me.btnCalculate
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CancelButton = Me.btnClear
+        Me.ClientSize = New System.Drawing.Size(609, 420)
+        Me.Controls.Add(Me.TableLayoutPanel1)
+        Me.Controls.Add(Me.lblTransactions)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblInput)
+        Me.Controls.Add(Me.tbxInput)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "frmMainMenu"
+        Me.Text = "CheckingApp"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.tblTransaction, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents rbnService As RadioButton
+    Friend WithEvents rbnDeposit As RadioButton
+    Friend WithEvents rbnCheck As RadioButton
+    Friend WithEvents tbxInput As TextBox
+    Friend WithEvents lblInput As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblTransactions As Label
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+    Friend WithEvents tblTransaction As DataSet
+End Class
---
+++ 04 CheckingApp/CheckingApp/Form1.vb
@@ -0,0 +1,31 @@
+﻿Public Class frmMainMenu
+    Private dtbTranactions
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        dtbTranactions = New DataTable("Transactions")
+        Dim colLable = New DataColumn()
+        Dim colAmount = New DataColumn()
+        dtbTranactions.Columns.Add(colLable)
+        dtbTranactions.Columns.Add(colAmount)
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        If (rbnCheck.Checked()) Then
+            Dim workRow As DataRow = dtbTranactions.NewRow()
+            workRow(0) = "Check"
+            workRow(1) = tbxInput
+            dtbTranactions.Rows.Add(workRow)
+        ElseIf (rbnDeposit.Checked()) Then
+
+        ElseIf (rbnService.Checked()) Then
+
+        Else
+            MsgBox("Please select a tranaction type.", vbOK + vbExclamation, "Transaction Error")
+        End If
+    End Sub
+
+    Private Sub rbnCheck_CheckedChanged(sender As Object, e As EventArgs) Handles rbnCheck.CheckedChanged
+
+    End Sub
+End Class
